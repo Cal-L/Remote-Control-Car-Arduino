7f9b40682ecb6ac5302cb4dd2092962b4e36312c
cleun007 <cleun007@gmail.com>
2015-05-07 19:30:05 -0700

    Inital commit

---
+++ Wireless_Motor_Reciever/Wireless_Motor_Reciever.ino
@@ -0,0 +1,20 @@
+int val;
+
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(9,OUTPUT);
+}
+void loop(){
+  if(Serial.available()>0){
+    val = Serial.read();
+  }
+  if(val > 0){
+    digitalWrite(9,HIGH);
+  }
+  else{
+    digitalWrite(9,LOW);
+  }
+  Serial.println(val);
+}
+
---
+++ Wireless_Motor_Sender/Wireless_Motor_Sender.ino
@@ -0,0 +1,16 @@
+int switchState = 0;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(3,INPUT);
+}
+void loop(){
+  switchState = digitalRead(3);
+  if(switchState == HIGH){
+    Serial.write(switchState);
+  }
+  else{
+    Serial.write(switchState);
+  }
+  delay(200);
+}
